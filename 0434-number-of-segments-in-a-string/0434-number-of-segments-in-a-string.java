2361ec78597d4ea25883c32d24dc0601605e8b8f
Ayushi Majumdar <majumdarayushi1505@gmail.com>
2026-01-30 02:28:49 +0530

    Time: 0 ms (100%), Space: 42.2 MB (97.57%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        boolean inWord = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                if (!inWord) {
+                    count++;
+                    inWord = true;
+                }
+            } else {
+                inWord = false;
+            }
+        }
+        return count;
+    }
+}
